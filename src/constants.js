6634dd9ba2c5aa51a8c639c5572a6fec5c527971
Mauricio Blum <mauriciocblum@gmail.com>
2021-10-25 16:21:30 +0200

    Added initial project files

---
+++ src/constants.js
@@ -0,0 +1,13 @@
+const CONTRACT_ADDRESS = '0x82A8e5aF8e5d5d6Cc421C5d4bac17629d2B233F5';
+
+const transformCharacterData = (characterData) => {
+  return {
+    name: characterData.name,
+    imageURI: characterData.imageURI,
+    hp: characterData.hp.toNumber(),
+    maxHp: characterData.maxHp.toNumber(),
+    attackDamage: characterData.attackDamage.toNumber(),
+  };
+};
+
+export { CONTRACT_ADDRESS, transformCharacterData };
